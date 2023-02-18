24b20f59154f73d8123ae75e3428f0c2157a24cc
DevHumbleChris <dev.humblechris254@gmail.com>
2023-02-18 23:27:46 +0300

    changed cors to defaults

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: 'http://localhost:5173',
-    credentials: true,
-  });
   await app.listen(3000);
 }
 bootstrap();
