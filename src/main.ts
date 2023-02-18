4d67f67a6c573d7a0d8b4d9e2f6e2ad0332b9336
DevHumbleChris <dev.humblechris254@gmail.com>
2023-02-18 23:33:43 +0300

    add enabled cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'https://book-store-ke.vercel.app/',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
