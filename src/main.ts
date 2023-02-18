cb04df4c214b1707d29259038cc43d85db6b4169
DevHumbleChris <dev.humblechris254@gmail.com>
2023-02-18 23:39:18 +0300

    changed cors to defaults

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: 'https://book-store-ke.vercel.app/',
-    credentials: true,
-  });
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
