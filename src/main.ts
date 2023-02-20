47166b2ebd1cb8073bb5f88ed582fdc6dcbe0e81
DevHumbleChris <dev.humblechris254@gmail.com>
2023-02-20 16:20:30 +0300

    added methods to the app.enamleCors method

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: 'https://book-store-ke.vercel.app',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
