f9eee7558a7d9a4e6110d21f0e38ec913a922540
DevHumbleChris <dev.humblechris254@gmail.com>
2023-02-20 16:25:59 +0300

    fixing cors again

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,21 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    allowedHeaders: ['content-type'],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: 'https://book-store-ke.vercel.app',
+  // app.enableCors({
+  //   allowedHeaders: ['content-type'],
+  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  //   origin: 'https://book-store-ke.vercel.app',
+  //   credentials: true,
+  // });
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
     credentials: true,
-  });
+  };
+
+  app.enableCors(options);
   await app.listen(3000);
 }
 bootstrap();
