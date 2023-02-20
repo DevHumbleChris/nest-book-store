5519959796b6a14172a20b71e11c6258b17deb67
DevHumbleChris <dev.humblechris254@gmail.com>
2023-02-20 17:13:43 +0300

    cors error fixing

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  // app.enableCors({
-  //   allowedHeaders: ['content-type'],
-  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  //   origin: 'https://book-store-ke.vercel.app',
-  //   credentials: true,
-  // });
-  const options = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  };
-
-  app.enableCors(options);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
